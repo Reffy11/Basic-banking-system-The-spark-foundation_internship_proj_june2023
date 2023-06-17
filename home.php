3d86e8e349d02cff48d0676e0f6855af18b6136a
Shriya Naik <shreyanaik527@gmail.com>
2023-06-17 15:06:17 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,51 @@
+<html>
+  <head>
+    <title>Spark BAnk</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <style>
+     body{
+    background:url('./img/615.jpg');
+    width: 100%;
+    height: 50px;
+    background-size: cover;
+    
+  }
+  .nav-item a{
+    font-size:40px ;
+    color:blue;
+  }
+    </style>
+  </head>
+
+    <center><h2>Spark bank</h2></center>
+    <div class="container">
+      <header class="d-flex justify-content-center py-3">
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <a href="home.php" class="nav-link" aria-current="page">Home</a>
+          </li>
+          <li class="nav-item"><a href="user.php" class="nav-link">Create User</a></li>
+          <li class="nav-item">
+            <a href="transfer.php" class="nav-link">Make a transfer</a>
+          </li>
+          <li class="nav-item">
+            <a href="thistory.php" class="nav-link">Transfer History</a>
+          </li>
+        </ul>
+      </header>
+      <p style="font-style: italic; font-size: 50px">"Spark Bank offer a safe and secure environment to store your money, providing peace of mind and protection"</P>
+    </div>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php">SPARKS BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+            <a href="home.php" class="nav-link" aria-current="page">Home</a>
+          </li>
+          <li class="nav-item"><a href="user.php" class="nav-link">Create User</a></li>
+          <li class="nav-item">
+            <a href="transfer.php" class="nav-link">Make A transfer</a>
+          </li>
+          <li class="nav-item">
+            <a href="thistory.php" class="nav-link">transfer History</a>
+          </li>
+          </div>
+       </nav>
